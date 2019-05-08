7e1c75619dcb9486586b5d649c9ae804a0f794f8
Sean Alvis <me@seanalvis.com>
2019-05-08 18:37:48 -0400

    initial commit of coffeerun

---
+++ scripts/datastore.js
@@ -0,0 +1,15 @@
+
+(function (window) {
+    'use strict';
+
+    var App = window.App || {};
+
+    function DataStore() {
+        console.log('running the DataStore function');
+        this.data = {};
+    }
+
+    App.DataStore = DataStore;
+    window.App = App;
+
+}) (window);
